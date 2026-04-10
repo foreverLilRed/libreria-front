b0965690ea07a40158c28fce21f4062af3d0976e
Pedro <projasma2002@gmail.com>
2026-04-09 19:19:34 -0500

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://github.com/foreverLilRed/libreria-front/',
   server: {
     allowedHosts: ['0ce9-190-108-93-43.ngrok-free.app', '44e4-190-108-93-43.ngrok-free.app']
   }
